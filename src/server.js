8d6b5ef161356c57c6e186e7ef1dcf3284c92275
Zhenghao Guo <z93guo@uwaterloo.ca>
2021-09-19 09:09:18 -0400

    Added auth and recipe image serving

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const recipeRoute = require('./routes/recipeRoute');
 const inventoryRoute = require('./routes/inventoryRoute');
 const { errorHandler, HttpError } = require('./utils/error');
+const auth = require('./services/auth.service');
 
 dotenv.config();
 
@@ -10,9 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/recipe-images', express.static('recipe-images'));
+app.use('/api/v1/*', auth);
 app.use('/api/v1/inventory', inventoryRoute);
 app.use('/api/v1/recipe', recipeRoute);
-app.use('*', (_req, _res, next) => {
+app.use('/api/v1/*', (_req, _res, next) => {
   next(new HttpError(404, 'Invalid route.'));
 });
 app.use(errorHandler);
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,3 +1,26 @@
 const { HttpError } = require('../utils/error');
 const fbAdmin = require('firebase-admin');
 const fbConfig = require('../../config/fbconfig.json');
+
+fbAdmin.initializeApp({ credential: fbAdmin.credential.cert(fbConfig)} );
+
+async function auth(req, _res, next) {
+  let isAuth = false;
+  if (req.headers.authorization) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const result = await fbAdmin.auth().verifyIdToken(token);
+      if (result.email) {
+        req.query.user = result.email;
+        isAuth = true;
+      }
+    } catch { }
+  }
+  if (!isAuth) {
+    return next(new HttpError(403, 'Unauthorized'));
+  } else {
+    next();
+  }
+}
+
+module.exports = auth;
